b1ab4a4a504b677f61f94b3f8f4c3c11b6f693f5
Abhishek's iMac <abhisheksimac@Abhishekss-iMac.local>
2018-08-13 08:57:44 +0530

    Relook - level 1 and level 2

--- Assets/Scripts/BallTouch.cs
+++ Assets/Scripts/BallTouch.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class BallTouch : MonoBehaviour {
 
@@ -9,10 +11,15 @@ public class BallTouch : MonoBehaviour {
 
 	public Camera camera;
 
-	public GameObject plane;
+	public Text scoreText;
 
-	private Rigidbody rigidBody;
+	public Button retryButton;
 
+	private Rigidbody2D rigidBody;
+
+	private int score;
+
+	private bool pressed = false;
 
 	public Transform righttop;
 	public Transform rightbottom;
@@ -22,13 +29,32 @@ public class BallTouch : MonoBehaviour {
 
 	void Start(){
 		
-		rigidBody = this.transform.GetComponent<Rigidbody> ();
+		rigidBody = this.GetComponent<Rigidbody2D> ();
 
+		//Instantiate(player, new Vector3(-5.69F, 0.87f, 26.3f), Quaternion.identity);
 
-		righttop.transform.position = Camera.main.ViewportToWorldPoint(new Vector3(1,1,0));
-		rightbottom.transform.position = Camera.main.ViewportToWorldPoint(new Vector3(1,0,0));
-		lefttop.transform.position = Camera.main.ViewportToWorldPoint(new Vector3(0,1,0));
-		leftbottom.transform.position = Camera.main.ViewportToWorldPoint(new Vector3(0,0,0));   
+		Time.timeScale = 1.3f;
+
+		scoreText.text = "Score : " + score;
+
+		if (Application.platform == RuntimePlatform.Android) {
+			print ("Android");
+
+			speed = 2.5f;
+
+			scoreText.text = "Android Score : " + score;
+		}
+
+		retryButton.gameObject.SetActive (false);
+
+		score = 0;
+
+		if (righttop != null) {
+			righttop.transform.position = Camera.main.ViewportToWorldPoint (new Vector3 (1, 1, 0));
+			rightbottom.transform.position = Camera.main.ViewportToWorldPoint (new Vector3 (1, 0, 0));
+			lefttop.transform.position = Camera.main.ViewportToWorldPoint (new Vector3 (0, 1, 0));
+			leftbottom.transform.position = Camera.main.ViewportToWorldPoint (new Vector3 (0, 0, 0));   
+		}
 
 
 	}
@@ -48,29 +74,77 @@ public class BallTouch : MonoBehaviour {
 
 			Vector3 forceVector = playerDeltaPosition - new Vector3( touchDeltaPosition.x, touchDeltaPosition.y, 0);
 
-			rigidBody.AddForce (forceVector * speed);
+			//forceVector = forceVector.normalized * 720f;
+
+			forceVector = forceVector.normalized * 690f;
+
+			Debug.Log ("Magnitude is " + forceVector.magnitude);
+
+			rigidBody.AddForce (forceVector);
 
 			// Move object across XY plane
 			//transform.Translate(-touchDeltaPosition.x * speed, -touchDeltaPosition.y * speed, 0);
 
 		}
 		
-		if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
-		{
-			// Get movement of the finger since last frame
-			Vector2 touchDeltaPosition = Input.GetTouch(0).deltaPosition;
+		if (Input.touchCount == 1 && Input.GetTouch (0).phase == TouchPhase.Moved && !pressed) {
 
-			Debug.Log ( " Touch " + touchDeltaPosition.x + " " + touchDeltaPosition.y);
+			pressed = true;
+
+			// Get movement of the finger since last frame
+			Vector2 touchDeltaPosition = Input.GetTouch (0).deltaPosition;
+
+			Debug.Log (" Touch " + touchDeltaPosition.x + " " + touchDeltaPosition.y);
 
 			Vector3 playerDeltaPosition = camera.WorldToScreenPoint (transform.position);
 
-			Debug.Log ( " Player " + playerDeltaPosition.x + " " + playerDeltaPosition.y);
+			Debug.Log (" Player " + playerDeltaPosition.x + " " + playerDeltaPosition.y);
 
-			Vector3 forceVector = playerDeltaPosition - new Vector3( touchDeltaPosition.x, touchDeltaPosition.y, 0);
+			Vector3 forceVector = playerDeltaPosition - new Vector3 (touchDeltaPosition.x, touchDeltaPosition.y, 0);
 
-			rigidBody.AddForce (forceVector * speed);
+			forceVector = forceVector.normalized * 720f;
 
+			scoreText.text = " Touch " + touchDeltaPosition.x + " " + touchDeltaPosition.y + " Player " + playerDeltaPosition.x + " " + playerDeltaPosition.y;
+
+			Debug.Log ("Magnitude is " + forceVector.magnitude);
+
+			rigidBody.AddForce (forceVector);
+
+		} 
+
+		if (Input.GetTouch (0).phase == TouchPhase.Ended) {
+			pressed = false;
 		}
+	}
+
+
+	private void OnCollisionEnter2D(Collision2D other)
+	{
+		if (other.collider.tag.Equals ("ScoreWall")) {
+			
+			score += 10;
+
+			scoreText.text = "Score : " + score;
+
+			Debug.Log ("scored");
+
+		} else if (other.collider.tag.Equals ("DangerWall")) {
+
+			Time.timeScale = 0f;
+
+			scoreText.text = " GAME OVER ";
+
+			retryButton.gameObject.SetActive (true);
+		} 
+	}
+
+
+
+	public void RestartGame(){
+
+		Time.timeScale = 1f;
+		SceneManager.LoadScene ("main2");
+
 	}
 
 
---
+++ Assets/Scripts/MoveCamera.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveCamera : MonoBehaviour {
+
+	public GameObject player;
+
+	private Vector3 offset;
+
+	void Start ()
+	{
+		offset = transform.position - player.transform.position;
+	}
+
+	void LateUpdate ()
+	{
+		Vector3 pos = new Vector3 (player.transform.position.x, -0f, 0f);
+		//transform.position = player.transform.position + offset;
+		transform.position =  pos + offset;
+	}
+}
